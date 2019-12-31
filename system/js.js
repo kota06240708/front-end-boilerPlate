66ca636a19ae9017a5aa481cbe7aff435a62c2a5
市川功太 <daiya50.ezweb.ne.jp@gmail.com>
2020-01-01 00:31:38 +0900

    add: jsのリロード処理を追加

--- system/js.js
+++ system/js.js
@@ -1,6 +1,43 @@
 import webpack from 'webpack'
 import webpackConfig from '../webpack.config.prod.babel'
 import chalk from 'chalk'
+import chokidar from 'chokidar'
+import glob from 'glob'
+import browserSync from 'browser-sync'
+
+import conf from './config'
+
+const { src, jsDir } = conf()
+
+export const relodaJs = () => {
+  return new Promise((resolve, reject) => {
+    glob(`${src}/**/${jsDir}`, {}, (error, dirs) => {
+      if (error) {
+        reject()
+      }
+
+      dirs.forEach(r => {
+        const watcher = chokidar.watch(r)
+        resolve()
+
+        watcher
+          .on('change', path => {
+            browserSync.reload()
+          })
+          .on('unlink', path => {
+            // ファイルが削除された時の処理
+            watcher.close() // watchを外す
+            console.log(chalk.yellow(`✔︎ Watcher Close ${path}`))
+          })
+          .on('error', path => {
+            watcher.close()
+            process.kill(process.pid, 'SIGHUP')
+            process.exit(0)
+          })
+      })
+    })
+  })
+}
 
 const onInitRender = () => {
   return new Promise(resolve => {
--- system/server.js
+++ system/server.js
@@ -1,9 +1,10 @@
 import css from './css'
 import html from './html'
 import browser from './browser'
+import { relodaJs } from './js'
 import checkDist from './config/util/check-dist'
 ;(async () => {
   await checkDist()
-  await Promise.all([html(), css()])
+  await Promise.all([html(), css(), relodaJs()])
   await browser()
 })()
